1d720dccfbaf01a5a8861a870ce34f1c8abd000a
laxsan <s.navalaxsan@gmail.com>
2020-05-10 09:30:54 +0200

    rajout de l'ajout des photo dans le process d'ajout d'annonce

--- routes/createAd.js
+++ routes/createAd.js
@@ -1,5 +1,17 @@
 createAdController = require('../controllers/createAd');
 
+const multer = require('multer');
+
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './uploads/buffer');
+     },
+    filename: function (req, file, cb) {
+        cb(null , file.originalname);
+    }
+});
+var upload = multer({storage: storage});
+
 module.exports = function(app) {
     app.use(function(req, res, next) {
       res.header(
@@ -10,5 +22,5 @@ module.exports = function(app) {
     });
       
 
-app.post('/api/adcreate/', createAdController.adCreate);
+app.post('/api/adcreate/',upload.array("file",9),createAdController.adCreate);
 }
